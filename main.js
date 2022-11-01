bc6b06cea0ac4874e0175267f7d048a3905a3767
Cards <marianne30011999@gmail.com>
2022-11-01 16:32:35 +0100

    Ready

---
+++ main.js
@@ -0,0 +1,46 @@
+let one='hello';
+console.log(one);
+let two='owu';
+console.log(two);
+let three='com';
+console.log(three);
+let four='ua';
+console.log(four);
+let five='ua';
+console.log(five);
+let six=1;
+console.log(six);
+let seven=10;
+console.log(seven);
+let eight=-999;
+console.log(eight);
+let ten=123;
+console.log(ten);
+let eleven=3.14;
+console.log(eleven);
+let twelve=2.7;
+console.log(twelve);
+let thirteen=16;
+console.log(thirteen);
+let fourteen=15>12;
+console.log(fourteen);
+let fifteen=15<12;
+console.log(fifteen);
+
+let firstName='Mariana';
+let middleName='Volodymyrivna';
+let lastName='Halych';
+let person=firstName+middleName+lastName;
+console.log(person);
+
+
+let a=100;
+let b='100';
+let c=true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+
+let name= prompt('Ваше імя','Mariana');
+console.log(name);
