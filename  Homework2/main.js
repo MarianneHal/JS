3ca2fa38a1430b76eff841cfc4fae4d075fae3b8
Cards <marianne30011999@gmail.com>
2022-11-03 09:46:10 +0100

    Ready2

---
+++  Homework2/main.js
@@ -0,0 +1,82 @@
+let x=['one','two','three',1,2,3,'four','five', 4,5];
+console.log(x[0]);
+console.log(x[1]);
+console.log(x[2]);
+console.log(x[3]);
+console.log(x[4]);
+console.log(x[5]);
+console.log(x[6]);
+console.log(x[7]);
+console.log(x[8]);
+console.log(x[9]);
+
+
+let one={
+    tittle: 'Moby-Dick',
+    pageCount: 427,
+    genre: 'Adventurefiction',
+    author: {
+        name: 'HermanMelville',
+        age: 203
+    }
+}
+
+let two={
+    tittle: 'The little Prince',
+    pageCount: 96,
+    genre: 'Fantasy',
+    author: {
+        name: 'Antonio de Saint-Exupery',
+        age: 122
+    }
+}
+
+let three={
+    tittle: 'TreeComrades',
+    pageCount: 499,
+    genre: 'War novel',
+    author: {
+        name: 'Erich Maria Remark',
+        age:  124
+    }
+}
+
+console.log(three.author.name);
+
+
+let five={
+    tittle: 'The little Prince',
+    pageCount: 96,
+    genre: 'Fantasy',
+    author: {
+        name: 'Antonio de Saint-Exupery',
+        age: 122
+    }
+}
+
+
+    let user1={
+       name: 'Oleg',
+       username: 'Aaa',
+       pasword: '123a'
+    }
+    let user2={
+        name: 'Igor',
+        username: 'Bbb',
+        pasword: '123b'
+    }
+    let user3={
+        name: 'Iryna',
+        username: 'Ccc',
+        pasword: '123c'
+    }
+
+    let users=[
+        user1,
+        user2,
+        user3
+    ]
+
+console.log(users[0].pasword);
+console.log(users[1].pasword);
+console.log(users[2].pasword);
